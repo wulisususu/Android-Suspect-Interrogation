9d79111f6caf4da498a5aac1b9933608a6546276
Mo Qiwen <330115138@qq.com>
2026-08-11 02:55:10 +0800

    load AI settings styles

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './styles.css'
+import './ai-settings.css'
 
 createApp(App).use(createPinia()).mount('#app')
